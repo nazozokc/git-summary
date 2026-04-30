ab8c65a2591c70091af259377f07ce79d017e2b9
nazozokc <nazozokc@gmail.com>
2026-04-30 20:03:52 +0900

    edit

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { consola } from "consola";
 import { cli } from "gunshi";
 import { branch } from "./branch.ts";
 import { upstream } from "./upstream.ts";
+import { status } from "./status.ts";
 
 await cli(process.argv.slice(2), {
   name: "git-summary",
   run: async (ctx) => {
     branch();
     upstream();
+    status();
   },
 });
--- src/status.ts
+++ src/status.ts
@@ -1,4 +1,7 @@
 import { consola } from "consola";
 import { execa } from "execa";
 
-export const status = async (): Promise<void> => {};
+export const status = async (): Promise<void> => {
+  const status = await execa("git", ["status", "--short"]);
+  consola.log(status.stdout);
+};
